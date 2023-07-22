32587b7658a6301f51500cee98ec8a3df31879e0
Brian <wamwangimathenge@gmail.com>
2023-07-22 13:19:45 +0300

    Solve Task 5
    This commit demonstrates how to solve Task 5

--- script.js
+++ script.js
@@ -40,3 +40,20 @@ while (i<=5) {
     i++
 }
 console.log("Counting completed!")
+
+// Write a "while" loop that will perform exactly the same repetitive code as this:
+
+// console.log(5)
+// console.log(4)
+// console.log(3)
+// console.log(2)
+// console.log(1)
+// console.log('Countdown finished!')
+
+var i = 5;
+
+while (i>0) {
+    console.log(i)
+    i--
+}
+console.log("Countdown finished!")
