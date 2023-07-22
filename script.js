3575e9e7083c9df9620042a973bcbc63bb330293
Brian <wamwangimathenge@gmail.com>
2023-07-22 13:22:46 +0300

    Solve Task 5
    This commit demonstrates how to solve Task 5

--- script.js
+++ script.js
@@ -57,3 +57,18 @@ while (i>0) {
     i--
 }
 console.log("Countdown finished!")
+
+// Write a "while" loop that will perform exactly the same repetitive code as this:
+
+// console.log(2018)
+// console.log(2019)
+// console.log(2020)
+// console.log(2021)
+// console.log(2022)
+
+var year = 2018;
+
+while (year <= 2022) {
+    console.log(year)
+    year++
+}
