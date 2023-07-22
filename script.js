692eecaa325dbbd8839887cea265a78cc40aa76e
Brian <wamwangimathenge@gmail.com>
2023-07-22 13:13:12 +0300

    Solve Task 2
    This commit demonstrates how to solve Task 2

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ for (i=1; i<=5; i++) {
     console.log(i)
 }
 console.log("Counting completed!")
+
+
+// Write a for loop that will perform exactly the same repetitive code as this:
+// console.log(5)
+// console.log(4)
+// console.log(3)
+// console.log(2)
+// console.log(1)
+// console.log('Countdown finished!')
+
+for (i=5; i>0; i--) {
+    console.log(i)
+}
+console.log("Countdown finished!")
