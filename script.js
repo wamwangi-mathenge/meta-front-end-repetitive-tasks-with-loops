6121b8e714322eb1cde5cba211bef7b51f3a0ea3
Brian <wamwangimathenge@gmail.com>
2023-07-22 13:15:15 +0300

    Solve Task 3
    This commit demonstrates how to solve Task 3

--- script.js
+++ script.js
@@ -24,3 +24,19 @@ for (i=5; i>0; i--) {
     console.log(i)
 }
 console.log("Countdown finished!")
+
+// Write a "while" loop that will perform exactly the same repetitive code as this:
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log('Counting completed!')
+
+i = 1;
+
+while (i<=5) {
+    console.log(i)
+    i++
+}
+console.log("Counting completed!")
