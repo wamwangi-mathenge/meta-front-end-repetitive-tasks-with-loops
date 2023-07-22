533968f710ef9f6c4fb6dbfd0ad1e0c72bdde2e0
Brian <wamwangimathenge@gmail.com>
2023-07-22 13:10:20 +0300

    Solve Task 1
    This commit demonstrates how to solve Task 1

---
+++ script.js
@@ -0,0 +1,12 @@
+// Write a "for" loop that will perform the same repetitive code as this:
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log('Counting completed!')
+
+for (i=1; i<=5; i++) {
+    console.log(i)
+}
+console.log("Counting completed!")
